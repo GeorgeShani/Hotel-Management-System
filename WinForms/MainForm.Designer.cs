91978bdce50c77291d773e78e7b0fbe1c9f656ee
GeorgeShani <shanidze.geo2004@gmail.com>
2026-06-02 11:52:12 +0400

    Initialize Backend

--- WinForms/MainForm.Designer.cs
+++ WinForms/MainForm.Designer.cs
@@ -34,7 +34,7 @@ partial class MainForm
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(1440, 800);
-        this.Text = "AI Knowledge Hub";
+        this.Text = "Hotel Management System";
     }
 
     #endregion
